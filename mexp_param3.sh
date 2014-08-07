39ef0cd4b68d260a0ce43b03f3a7c62cfc0efc8a
Antoine Frénoy <frenoy@gmx.com>
2014-08-07 14:54:19 +0200

    same experiments for higher values of m

---
+++ mexp_param3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.5mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.5mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 200, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.3mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.3mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 200, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.7mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.7mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 200, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.4mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.4mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 200, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.6mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.6mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 200, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.8mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/param3/c0.8mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 200, false, true );" j$RANDOM
