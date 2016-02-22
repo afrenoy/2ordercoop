5a0b8b56e76ff9715f56ba944982e50118c2d326
Antoine Frénoy <afrenoy@gmx.com>
2016-02-22 19:11:23 +0100

    Remove old scripts for the first dataset (exp1) and replace them by newer scripts that should produce the same output.
    This permits consitency between different datasets.

---
+++ exp1.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp1', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp2', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp5', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp10', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp20', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp50', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.2mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.075, 0.2, 1.125, 200, false, true );" j$RANDOM
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp1', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp2', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp5', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp10', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp20', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp50', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.3mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.175, 0.3, 1.125, 200, false, true );" j$RANDOM
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp1', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp2', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp5', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp10', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp20', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp50', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.4mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.275, 0.4, 1.125, 200, false, true );" j$RANDOM
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp1', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp2', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp5', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp10', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp20', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp50', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.5mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.375, 0.5, 1.125, 200, false, true );" j$RANDOM
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp1', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp2', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp5', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp10', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp20', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp50', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.6mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.475, 0.6, 1.125, 200, false, true );" j$RANDOM
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp1', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp2', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp5', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp10', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp20', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp50', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.7mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.575, 0.7, 1.125, 200, false, true );" j$RANDOM
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp1', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp2', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp5', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp10', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp20', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp50', 5, 5000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.8mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.675, 0.8, 1.125, 200, false, true );" j$RANDOM
+
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp1', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 1, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp2', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 2, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp5', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 5, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp10', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 10, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp20', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 20, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp50', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 50, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp100', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 100, false, true );" j$RANDOM
+./launchmatlabcommand.sh "repls2( '/sps/inter/liris/afrenoy/S2/exp1/c0.9mexp200', 5, 2000, 100, 100, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.775, 0.9, 1.125, 200, false, true );" j$RANDOM
