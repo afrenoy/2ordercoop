c2c087f6a8e39b89612c210075f2cfd0948876eb
Antoine Frénoy <afrenoy@gmx.com>
2016-02-23 14:06:13 +0100

    In control1 (figure 2) add a secondary axis on the right side of the figure to make it more clear what is log scale and what is not

--- plotcontrol1.m
+++ plotcontrol1.m
@@ -12,11 +12,12 @@ n=0;
 listem=[1 2 5 10 20 50 100 200];
 listec=[0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9];
 
-hblank=0.035; % What horizontal space to leave between two subplots
+hblank=0.015; % What horizontal space to leave between two subplots
 vblank=0.015; % What vertical space to leave between two subplots
 
-saveh=0.07; % What horizontal space to leave at the left of the figure for 'global' labels
+saveh=0.11; % What horizontal space to leave at the left of the figure for 'global' labels
 savev=0.16; % What vertical space to leave at the bottom of the figure for 'global' labels
+savehr=0.03;
 
 hright=0.01;
 
@@ -31,12 +32,12 @@ for m=listem
         d1=['~/data/evomut/exp1/c' num2str(c) 'mexp' num2str(m)];
         d2=['~/data/evomut/exp1_ctl/c' num2str(c) 'mexp' num2str(m)];
         d3=['~/data/evomut/exp1_cth/c' num2str(c) 'mexp' num2str(m)];
-        left=saveh+(nc*(1-saveh)-1)/numel(listec)+hblank/2;
+        left=saveh+(nc*(1-saveh-savehr)-1)/numel(listec)+hblank/2;
         bottom=savev+(nm*(1-savev)-1)/numel(listem)+vblank/2;
-        width=(1-saveh)/numel(listec)-hblank;
+        width=(1-saveh-savehr)/numel(listec)-hblank;
         height=(1-savev)/numel(listem)-vblank;
         subplot('Position',[left bottom width height]);
-        [ax1,h1,h4,h5]=plotcontrollog(d1,d2,d3,'',lw1,lw2);
+        [ax1,h1,h4,h5]=plotcontrollogbis(d1,d2,d3,'',lw1,lw2);
         set(ax1,'FontSize',fs1);
         set(ax1,'LineWidth',lw1);
         set(ax1,'color','none');
@@ -44,11 +45,11 @@ for m=listem
         set(ax1,'YMinorTick','off');
         set(ax1,'Box','off');
         if c==min(listec)
-            %set(ax1,'YAxisLocation','left');
+            set(ax1,'YAxisLocation','left');
         elseif c==max(listec)
-            %set(ax1,'YAxisLocation','right');
+            set(ax1,'YAxisLocation','right');
         else
-            %set(ax1,'YColor','none');
+            set(ax1,'YColor','none');
         end
     end
 end
@@ -60,7 +61,7 @@ bottomax=axes('Position',[saveh 0 1 savev],'color','none','Xtick',[],'Ytick',[],
 nm=0;
 for m=listem
     nm=nm+1;
-    t=text(0.4,(nm*(1-savev)-1)/numel(listem)+vblank/2+((1-savev)/numel(listem)-vblank)/2-0.04,['m = ' num2str(m)],'FontSize',fs1,'FontWeight','normal','rotation',90);
+    t=text(0.15,(nm*(1-savev)-1)/numel(listem)+vblank/2+((1-savev)/numel(listem)-vblank)/2-0.04,['m = ' num2str(m)],'FontSize',fs1,'FontWeight','normal','rotation',90);
     set(t,'Parent',leftax);
 end
 
@@ -71,8 +72,8 @@ for c=listec
     set(t,'Parent',bottomax);
 end
 
-% Create axes for labels and parameter names
-new=axes('Position',[0.015 0.025 1-hright 1]);
+% Create axes for labels, parameter names and background shading
+new=axes('Position',[0.015 0.025 1-hright 1],'Clipping','off');%,'OuterPosition',[-0.3 -0.3 1.6 1.6]);
 set(new, 'color', 'none');
 set(new, 'XTick',[]);
 set(new, 'YTick',[]);
@@ -101,14 +102,15 @@ set(hl,'Position',pl)
 
 
 % Draw parameter zones
-ini=-0.002;
+ini=-0.06;
 %fin=1.02;
-fin=1.02;
-pa=patch(0.001+saveh/2+[ini 0.125 0.125 0.25 0.25 0.375 0.375 0.25 0.25 ini ini]*(1-saveh), 0.035+savev/2+[0.25 0.25 0.375 0.375 0.5 0.5 0.75 0.75 1 1 0.25]*(1-savev),[216.6667 230.0000 246.6667]/255,'LineStyle','none');
-pb=patch(0.001+saveh/2+[ini 0.125 0.125 0.375 0.375 0.875 0.875 fin fin 0.25 0.25 0.375 0.375 0.25 0.25 0.125 0.125 ini ini]*(1-saveh), 0.035+savev/2+[0.125 0.125 0.25 0.25 0.375 0.375 0.5 0.5 1 1 0.75 0.75 0.5 0.5 0.375 0.375 0.25 0.25 0.125]*(1-savev),[226.6667 255 226.6667]/255,'LineStyle','none');
-pc=patch(0.001+saveh/2+[ini fin fin 0.875 0.875 0.375 0.375 0.125 0.125 ini ini]*(1-saveh), 0.035+savev/2+[0 0 0.5 0.5 0.375 0.375 0.25 0.25 0.125 0.125 0]*(1-savev),[242.2222 214.4444 246.6667]/255,'LineStyle','none');
+fin=1.06;
+pa=patch(0.025+saveh/2+[ini 0.125 0.125 0.25 0.25 0.375 0.375 0.25 0.25 ini ini]*(1-saveh-savehr), 0.035+savev/2+[0.25 0.25 0.375 0.375 0.5 0.5 0.75 0.75 1 1 0.25]*(1-savev),[216.6667 230.0000 246.6667]/255,'LineStyle','none');
+pb=patch(0.025+saveh/2+[ini 0.125 0.125 0.375 0.375 0.875 0.875 fin fin 0.25 0.25 0.375 0.375 0.25 0.25 0.125 0.125 ini ini]*(1-saveh-savehr), 0.035+savev/2+[0.125 0.125 0.25 0.25 0.375 0.375 0.5 0.5 1 1 0.75 0.75 0.5 0.5 0.375 0.375 0.25 0.25 0.125]*(1-savev),[226.6667 255 226.6667]/255,'LineStyle','none');
+pc=patch(0.025+saveh/2+[ini fin fin 0.875 0.875 0.375 0.375 0.125 0.125 ini ini]*(1-saveh-savehr), 0.035+savev/2+[0 0 0.5 0.5 0.375 0.375 0.25 0.25 0.125 0.125 0]*(1-savev),[242.2222 214.4444 246.6667]/255,'LineStyle','none');
 uistack(new,'bottom');
+%set(new, 'Xlim',[0 1]);
 
 %% Export and fix
-print('-depsc','-loose','data1muteffect');
+%print('-depsc','-loose','data1muteffect');
 print('-dpdf','-loose','data1muteffect');
