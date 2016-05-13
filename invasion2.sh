be6625e94ab946b3eddb8cee3419531d471abf00
Antoine Frénoy <afrenoy@gmx.com>
2016-05-13 19:21:24 +0200

    Add a set of ecological invasion experiments

---
+++ invasion2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_0/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 1, false, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_0/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 100, false, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_1/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 1, false, 1, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_1/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 100, false, 1, false );" 
+
+# liquid
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_0/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 1, true, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_0/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 100, true, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_1/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 1, true, 1, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_1/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0.9, 1.125, 100, true, 1, false );" 
+
+# neutral
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_0_neutral/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 1, false, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_0_neutral/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 100, false, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_1_neutral/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 1, false, 1, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasion_1_neutral/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 100, false, 1, false );" 
+
+# liquid neutral
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_0_neutral/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 1, true, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_0_neutral/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 100, true, 0, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_1_neutral/c0.9mexp1', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 1, true, 1, false );" 
+./launchmatlabcommandrnd.sh "repls2invasion( '/sps/inter/liris/afrenoy/S2/exp2invasionl_1_neutral/c0.9mexp100', 100, 200, 20, 20, 0.001, 0.001, 0.1, 0.1, 0.775, 0, 0, 100, true, 1, false );" 
