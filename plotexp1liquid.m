874a5f56d7f4e72cc2acfa829b2046ab51908676
Antoine Frénoy <afrenoy@gmx.com>
2017-01-05 18:07:39 +0100

    Add code to produce 'liquid' control

---
+++ plotexp1liquid.m
@@ -0,0 +1,20 @@
+%PLOTEXP1LIQUID produces control for exp 1 liquid
+
+%% Global parameters
+load('graphicparamsv1');
+ini=-0.017;
+fin=1.011;
+saveh=0.07;
+savev=0.1;
+
+%% Main plot
+plotexp('~/data/evomut/liquid/exp1l/');
+
+%% Draw parameter zones
+pa=patch(0.013+saveh/2+[ini 0.125 0.125 0.25 0.25 0.375 0.375 0.25 0.25 ini ini]*(1-saveh), 0.011+savev/2+[0.25 0.25 0.375 0.375 0.5 0.5 0.75 0.75 1 1 0.25]*(1-savev),colorb1,'LineStyle','none');
+pb=patch(0.013+saveh/2+[ini 0.125 0.125 0.375 0.375 0.875 0.875 fin fin 0.25 0.25 0.375 0.375 0.25 0.25 0.125 0.125 ini ini]*(1-saveh), 0.011+savev/2+[0.125 0.125 0.25 0.25 0.375 0.375 0.5 0.5 1 1 0.75 0.75 0.5 0.5 0.375 0.375 0.25 0.25 0.125]*(1-savev),colorb2,'LineStyle','none');
+pc=patch(0.013+saveh/2+[ini fin fin 0.875 0.875 0.375 0.375 0.125 0.125 ini ini]*(1-saveh), 0.011+savev/2+[0 0 0.5 0.5 0.375 0.375 0.25 0.25 0.125 0.125 0]*(1-savev),colorb3,'LineStyle','none');
+
+%% Export and fix
+print('-depsc','-loose','liquid1');
+print('-dpdf','-loose','liquid1');
