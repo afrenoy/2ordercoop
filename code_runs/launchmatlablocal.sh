e97bd4da908986361607138ef7290eb86ee38773
Antoine Frénoy <afrenoy@gmx.com>
2017-05-06 17:34:01 +0200

    In launchmatlablocal.sh, add code_analysis to path

--- code_runs/launchmatlablocal.sh
+++ code_runs/launchmatlablocal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-p=$(pwd)/../code_model/
-/Applications/MATLAB_R2016b.app/Contents/MacOS/MATLAB_maci64 -nojvm -nodisplay -r "path(path,'$p');$1;exit()"
+p=$(pwd)
+/Applications/MATLAB_R2016b.app/Contents/MacOS/MATLAB_maci64 -r "path(path,'$p/../code_model/');path(path,'$p/../code_analysis/');$1;exit()"
 
